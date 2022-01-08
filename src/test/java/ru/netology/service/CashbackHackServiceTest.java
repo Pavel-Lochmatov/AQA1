72404d1d0e2e22e1bd11d059ea18a7234f0c5a9b
Pavel-Lokhmatov <hlest.ivsvnyby@gmail.com>
2022-01-08 18:43:20 +0400

    aqa1

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,41 +1,4 @@
-package ru.netology.service;
-
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
-
+import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
-
-    @Test
-    public void testRemain() {
-        int amount = 900;
-        int expected = 100;
-
-        CashbackHackService сashbackHackService = new CashbackHackService();
-        int actual = сashbackHackService.remain(amount);
-        assertEquals(actual, expected);
-
-    }
-    @Test
-    public void testRemainMoAmount1000() {
-        int amount = 1500;
-        int expected = 500;
-
-        CashbackHackService сashbackHackService = new CashbackHackService();
-        int actual = сashbackHackService.remain(amount);
-        assertEquals(actual, expected);
-
-    }
-    @Test
-    public void testRemainAmount1000() {
-        int amount = 1000;
-        int expected = 0;
-
-        CashbackHackService сashbackHackService = new CashbackHackService();
-        int actual = сashbackHackService.remain(amount);
-        assertEquals(actual, expected);
-
-    }
-
+  
 }
-
